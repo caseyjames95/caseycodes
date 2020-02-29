8e82c32b7c53cf3c9107945251dbaae1a19fbf46
caseyjames95 <caseypurves@gmail.com>
2020-02-29 10:26:39 -0800

    Working react github calendar

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 
 // Import components
 import NavBar from '../src/components/NavBar'
+// import GitHubCal from './components/GitHubCal'
+import GitHubCalendar from 'react-github-calendar'
 
 
 
 function App() {
   return (
-    <>
+    <div className="container">
       <NavBar />
-    </>
+    </div>
   );
 }
 
