2bcedb6effbc698a8df2bc18a2b2574ed202830d
caseyjames95 <caseypurves@gmail.com>
2020-02-29 10:05:52 -0800

    Working background
    Background Images are not working properly.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import '../src/styles/index.css'
 import './App.css';
 
 // Import components
 import NavBar from '../src/components/NavBar'
 
+
+
 function App() {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+    </>
   );
 }
 
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -15,16 +15,16 @@ const NavBar = (props) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggle = () => setIsOpen(!isOpen);
-
+    
     return (
-        <div>
+        <div className="container">
             <Navbar color="clear" light expand="md">
                 <NavbarBrand id="navTitle" href="/">Casey<strong>Codes</strong></NavbarBrand>
                 <NavbarToggler onClick={toggle} />
                 <Collapse isOpen={isOpen} navbar>
                     <Nav className="mr-auto" navbar>
                         <NavItem>
-                            <NavLink id="navSection" href="/">About</NavLink>
+                            <NavLink id="navSection" href="/">Home</NavLink>
                         </NavItem>
                         <NavItem>
                             <NavLink id="navSection" href="/">Web Dev</NavLink>
